73cabcc1eb0373a86f1004c07177d004a6d1d419
faya.shz <faya.shz@alibaba-inc.com>
2019-07-06 16:08:28 +0800

    friend func

---
+++ src/dm04/dm02_friend_fun.cpp
@@ -0,0 +1,34 @@
+//
+// Created by bruce on 2019-07-06.
+//
+#include <iostream>
+
+using namespace std;
+
+
+class A {
+public:
+    A(int a) {
+        this->a = a;
+    }
+
+    friend void modifyA(A *a, int data);
+
+    int getA() {
+        return this->a;
+    }
+
+private:
+    int a;
+};
+
+void modifyA(A *a1, int data) {
+    a1->a = data;
+}
+
+
+int main() {
+    A a1(100);
+    modifyA(&a1, 200);
+    cout << "a = " << a1.getA() << endl;
+}
